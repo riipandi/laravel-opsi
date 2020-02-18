409d218605a4a09a16ce83b50b652204878dcc4f
Aris Ripandi <ar.is@outlook.com>
2020-02-19 04:45:02 +0700

    add opsi:get command

--- src/Console/OpsiGetCommand.php
+++ src/Console/OpsiGetCommand.php
@@ -5,21 +5,21 @@ namespace Riipandi\LaravelOpsi\Console;
 use Riipandi\LaravelOpsi\OpsiFacade as Opsi;
 use Illuminate\Console\Command;
 
-class OpsiSetCommand extends Command
+class OpsiGetCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'opsi:set {key : Setting key} {value : Setting value}';
+    protected $signature = 'opsi:get {key : Setting key}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create a setting value';
+    protected $description = 'Get a setting value';
 
     /**
      * Execute the console command.
@@ -28,8 +28,12 @@ class OpsiSetCommand extends Command
      */
     public function handle()
     {
-        Opsi::set($this->argument('key'), $this->argument('value'));
+        $result = Opsi::get($this->argument('key'));
 
-        $this->info('Setting value created.');
+        if (!$result) {
+            $this->error('No option with that key!');
+        }
+
+        $this->info($result);
     }
 }
